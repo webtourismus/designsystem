e91c5ff21f96bdfdd9a4c52e13117c5022ea03c8
Webtourismus.at <entwicklung@webtourismus.at>
2023-09-26 16:23:31 +0200

    Bugfix SplideJS arrows in Firefox

--- js/splide.js
+++ js/splide.js
@@ -6,11 +6,14 @@
           let splideInstance = new Splide(elem);
           splideInstance.on('arrows:mounted arrows:updated', function() {
             try {
-              const firstImage = splideInstance.Components.Elements.slides[0].querySelector('img');
-              if (firstImage.height) {
-                splideInstance.Components.Arrows.arrows.prev.style.top = (firstImage.height / 2) + 'px';
-                splideInstance.Components.Arrows.arrows.next.style.top = (firstImage.height / 2) + 'px';
-              }
+              const firstImage = splideInstance.Components.Elements.slides[0].querySelector('picture, img');
+              /* setTimeout due race condition in firefox with responsive images */
+              window.setTimeout(function() {
+                if (firstImage.height > 0) {
+                  splideInstance.Components.Arrows.arrows.prev.style.top = (firstImage.height / 2) + 'px';
+                  splideInstance.Components.Arrows.arrows.next.style.top = (firstImage.height / 2) + 'px';
+                }
+              }, 50);
             } catch (e) {/*silent fail*/}
           });
           splideInstance.mount();
