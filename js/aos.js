2158dd9c907c70c3b82236a7a5c21b8c48f7cf1d
Webtourismus.at <entwicklung@webtourismus.at>
2023-09-26 10:03:18 +0200

    Fix aos startEvent

--- js/aos.js
+++ js/aos.js
@@ -3,7 +3,7 @@
     attach: function attach(context, settings) {
       once('aos', document.documentElement, context).forEach(elem => {
         if (!drupalSettings.path.currentPathIsAdmin) {
-          AOS.init(drupalSettings?.aos ?? {startEvent: 'load', duration: 1000})
+          AOS.init(drupalSettings?.aos ?? {duration: 1000})
         }
       });
     },
