a5c5d02fb6a4377730d75271ed93e5c086c2a72e
Webtourismus.at <entwicklung@webtourismus.at>
2023-11-14 12:22:49 +0100

    Remove LayoutIconBuilder, JS onload event for flatpickr, fix incorrect viewmode hint of field templates

--- src/DesignHelper.php
+++ src/DesignHelper.php
@@ -24,4 +24,26 @@ class DesignHelper {
     }
     return new Attribute($attribute ?? []);
   }
+
+  /**
+   * Returns the real name of the used view mode of a field render array.
+   *
+   * Drupal core provides $fieldRenderArray['element']['#view_mode'].
+   * But this variable might not contain the real name of the view mode used for rendering in some scenarios:
+   * - The field was rendered programmatically / in isolation (e.g. "ebr_teaser" module).
+   * - The "view_mode_switch" module might change the view mode.
+   * - The entity was rendered by core "views" module (entire views row rendered as entity with entity view mode).
+   * In those cases, the view mode might be e.g. "_custom" or "default" (by intention due caching or simply by mistake).
+   * This function tries to find the real view name using known workarounds.
+   *
+   * @return string
+   */
+  public static function getRealViewmode(array $renderArray): string {
+    $viewMode = $renderArray['element']['#view_mode'];
+    $entity = $renderArray['element']['#object'];
+    if ($viewMode == 'default' && $entity?->hasField('field_viewmode') && !$entity->get('field_viewmode')->isEmpty()) {
+      $viewMode = $entity->get('field_viewmode')->value;
+    }
+    return $viewMode;
+  }
 }
