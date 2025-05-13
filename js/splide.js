432ee41df9c40c44ac2a98dbb6ec439e12b05cf0
Webtourismus.at <entwicklung@webtourismus.at>
2025-05-13 16:52:21 +0200

    Fixed arrow on resize and if no arrows set

--- js/splide.js
+++ js/splide.js
@@ -3,14 +3,19 @@
     attach: function attach(context, settings) {
       if (!drupalSettings.path.currentPathIsAdmin) {
         function addSplideEvents(splideInstance) {
-          splideInstance.on('arrows:mounted arrows:updated', function () {
+          splideInstance.on('resized arrows:mounted arrows:updated', function () {
             try {
               const firstImage = splideInstance.Components.Elements.slides[0].querySelector('picture, img');
               /* setTimeout due race condition in firefox with responsive images */
               window.setTimeout(function () {
                 if (firstImage?.height > 0) {
-                  splideInstance.Components.Arrows.arrows.prev.style.top = (firstImage.height / 2) + 'px';
-                  splideInstance.Components.Arrows.arrows.next.style.top = (firstImage.height / 2) + 'px';
+                  if (splideInstance.Components.Arrows.arrows.prev) {
+                    splideInstance.Components.Arrows.arrows.prev.style.top = (firstImage.height / 2) + 'px';
+                  }
+
+                  if (splideInstance.Components.Arrows.arrows.next) {
+                    splideInstance.Components.Arrows.arrows.next.style.top = (firstImage.height / 2) + 'px';
+                  }
                 }
               }, 50);
             } catch (e) {/*silent fail*/ }
@@ -20,7 +25,7 @@
         function dispatchCreatedEvent(splideInstance) {
           let ev = new CustomEvent('splide:mounted', { detail: { splide: splideInstance } });
           document.dispatchEvent(ev);
-        }
+        };
 
         once('splide', '.splide', context).forEach(elem => {
           if (elem.dataset.isNavigation) {
